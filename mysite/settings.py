dd26b59667cea664608f1da6dba4454a23ee2700
rudra <rudramuduli2001@gmail.com>
2023-01-02 15:48:29 +0530

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wh-2&@dzlv#%28ju9&f9t3-60f&e8y-t$98ntfvpw9m1b8y(29'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['157.245.107.120']
 
