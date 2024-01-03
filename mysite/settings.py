7e80eb3855192d2fa6555830b2d781256fe6cce1
Rudranarayan Muduli <84215414+iamrudra-narayan@users.noreply.github.com>
2024-01-03 16:20:27 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.parse('''postgres://rudra:w4maL1vGAIG1yXJsYLzAT6fMi2EsGJCH@dpg-cmajhc7109ks73ff9et0-a/rudrazomato_kh7g''')
+    'default': dj_database_url.parse('''postgres://rudra:w4maL1vGAIG1yXJsYLzAT6fMi2EsGJCH@dpg-cmajhc7109ks73ff9et0-a.oregon-postgres.render.com/rudrazomato_kh7g''')
     
 }
 
